93f9eff3d90c5c2e5fe87100bd1992fae228f030
Peter Varga <pvarga@inf.u-szeged.hu>
2015-11-17 21:44:32 +0100

    Simplify lcdsim

--- src/lcdsim.c
+++ src/lcdsim.c
@@ -3,85 +3,49 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-static unsigned char cg_ram[CG_RAM_SIZE];
-static unsigned char dd_ram[DD_RAM_SIZE];
-static unsigned char dd_ram2[DD_RAM_SIZE2];
-
-static int base;
-static int offset;
+#define RAM_SIZE CG_RAM_SIZE + DD_RAM_SIZE + DD_RAM_SIZE2
+static unsigned char lcd_ram[RAM_SIZE];
+static unsigned char *ram_ptr;
 
 void lcd_init()
 {
-    for (int i = 0; i < CG_RAM_SIZE; ++i)
-        cg_ram[i] = 0;
-    for (int i = 0; i < DD_RAM_SIZE; ++i)
-        dd_ram[i] = ' ';
-    for (int i = 0; i < DD_RAM_SIZE2; ++i)
-        dd_ram2[i] = ' ';
+    for (int i = 0; i < RAM_SIZE; ++i)
+        lcd_ram[i] = 0;
 
-    base = -1;
-    offset = 0;
+    ram_ptr = 0;
 }
 
 void lcd_send_command(int command)
 {
-    if (command >= CG_RAM_ADDR && command < CG_RAM_ADDR + CG_RAM_SIZE) {
-        base = CG_RAM_ADDR;
-        offset = command - CG_RAM_ADDR;
-        return;
+    if (command < 0 || command >= RAM_SIZE) {
+        fprintf(stderr, "Invalid command: 0x%x\n", command);
+        exit(1);
     }
 
-    if (command >= DD_RAM_ADDR && command < DD_RAM_ADDR + DD_RAM_SIZE) {
-        base = DD_RAM_ADDR;
-        offset = command - DD_RAM_ADDR;
-        return;
-    }
-
-    if (command >= DD_RAM_ADDR2 && command < DD_RAM_ADDR2 + DD_RAM_SIZE2) {
-        base = DD_RAM_ADDR2;
-        offset = command - DD_RAM_ADDR2;
-        return;
-    }
-
-    fprintf(stderr, "Invalid command: 0x%x\n", command);
-    exit(1);
+    ram_ptr = &lcd_ram[command];
 }
 
 void lcd_send_data(unsigned char data) {
-    if (base < 0) {
+    if (!ram_ptr) {
         fprintf(stderr, "LCD command has not been sent!\n");
         exit(1);
     }
 
-    unsigned char *ram = 0;
-
-    switch (base) {
-    case CG_RAM_ADDR:
-        ram = (void *)&cg_ram;
-        offset %= 8 * 8;
-        break;
-    case DD_RAM_ADDR:
-        ram = (void *)&dd_ram;
-        offset %= DD_RAM_SIZE;
-        break;
-    case DD_RAM_ADDR2:
-        ram = (void *)&dd_ram2;
-        offset %= DD_RAM_SIZE2;
-        break;
-    default:
-        fprintf(stderr, "Invalid base pointer: 0x%x\n", base);
+    if ((void *)ram_ptr >= (void *)(&lcd_ram + RAM_SIZE)) {
+        fprintf(stderr, "Memory Overflow!");
         exit(1);
     }
 
-    ram[offset++] = data;
+    *ram_ptr = data;
+    ram_ptr++;
 }
 
-void lcd_sim_print_line(unsigned char *ram, int ram_size)
+void lcd_sim_print_line(unsigned char *dd_ram, int dd_ram_size)
 {
     for (int row = 0; row < 8; ++row) {
-        for (int i = 0; i < ram_size; ++i) {
-            unsigned char ch = ram[i];
-            unsigned char pattern = cg_ram[ch * 8 + row];
+        for (int i = 0; i < dd_ram_size; ++i) {
+            unsigned char ch = dd_ram[i];
+            unsigned char pattern = lcd_ram[CG_RAM_ADDR + ch * 8 + row];
             for (int s = 7; s > 2; --s) {
                 if ((pattern >> s) & 1)
                     putchar('*');
@@ -97,9 +61,7 @@ void lcd_sim_print_line(unsigned char *ram, int ram_size)
 
 void lcd_sim_print()
 {
-    lcd_sim_print_line(dd_ram, DD_RAM_SIZE);
+    lcd_sim_print_line(&lcd_ram[DD_RAM_ADDR], DD_RAM_SIZE);
     printf("\n");
-    lcd_sim_print_line(dd_ram2, DD_RAM_SIZE2);
+    lcd_sim_print_line(&lcd_ram[DD_RAM_ADDR2], DD_RAM_SIZE2);
 }
-
-
